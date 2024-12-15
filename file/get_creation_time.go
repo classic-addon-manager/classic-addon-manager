0d0e4e0d10ba0f52df558ab1213aa9bfe9d942e3
ProjectInfinity <asjur@proton.me>
2024-12-15 04:48:25 +0100

    Make GetModifiedTime platform independent (this opens up for Linux and macOS support, kind of)

--- file/get_creation_time.go
+++ file/get_creation_time.go
@@ -2,20 +2,13 @@ package file
 
 import (
 	"os"
-	"syscall"
 	"time"
 )
 
-// GetModifiedTime - This function only functions on Windows
 func GetModifiedTime(path string) (time.Time, error) {
 	info, err := os.Stat(path)
 	if err != nil {
 		return time.Time{}, err
 	}
-
-	if stat, ok := info.Sys().(*syscall.Win32FileAttributeData); ok {
-		return time.Unix(0, stat.LastWriteTime.Nanoseconds()), nil
-	}
-
-	return time.Time{}, nil
+	return info.ModTime(), nil
 }
