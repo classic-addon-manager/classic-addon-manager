ccd0a72370269447592b4a7cefca9a78cab819c1
ProjectInfinity <asjur@proton.me>
2025-01-15 21:45:30 +0100

    Update dependencies

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,17 @@
 // @ts-ignore
 import path from 'path';
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {defineConfig} from 'vite'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      $lib: path.resolve("./src/lib"),
-      $stores: path.resolve("./src/stores"),
-      $screens: path.resolve("./src/screens"),
-    },
-  }
-})
+export default defineConfig(({mode}) => ({
+    plugins: [svelte()],
+    resolve: {
+        conditions: mode === 'test' ? ['browser'] : [],
+        alias: {
+            $lib: path.resolve("./src/lib"),
+            $stores: path.resolve("./src/stores"),
+            $screens: path.resolve("./src/screens"),
+        },
+    }
+}));
