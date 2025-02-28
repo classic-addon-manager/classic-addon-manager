f00f829a2fed138cd008aee649e9ab3f36b5acbd
ProjectInfinity <asjur@proton.me>
2025-03-01 00:05:25 +0100

    Cleanup

--- backend/app/app.go
+++ backend/app/app.go
@@ -2,7 +2,6 @@ package app
 
 import (
 	"ClassicAddonManager/backend/addon"
-	"ClassicAddonManager/backend/config"
 	"ClassicAddonManager/backend/github"
 	"ClassicAddonManager/backend/logger"
 	"ClassicAddonManager/backend/shared"
@@ -18,30 +17,8 @@ type App struct {
 	addonManifest   []addon.AddonManifest
 }
 
-// NewApp creates a new App application struct
-func NewApp(version string) *App {
-	shared.Version = version
-	return &App{}
-}
-
 func (a *App) GetVersion() string {
 	return shared.Version
-}
-
-func (a *App) GetConfigString(key string) string {
-	return config.GetString(key)
-}
-
-func (a *App) SetConfigString(key string, value string) {
-	config.SetString(key, value)
-}
-
-func (a *App) GetConfigBool(key string) bool {
-	return config.GetBool(key)
-}
-
-func (a *App) SetConfigBool(key string, value bool) {
-	config.SetBool(key, value)
 }
 
 func (a *App) SelectDirectory() string {
@@ -53,10 +30,6 @@ func (a *App) SelectDirectory() string {
 		return ""
 	}
 	return dir
-}
-
-func (a *App) GetAddonManifests() []addon.AddonManifest {
-	return addon.GetAddonManifest()
 }
 
 func (a *App) GetReleases(repo string) ([]github.GithubRelease, error) {
