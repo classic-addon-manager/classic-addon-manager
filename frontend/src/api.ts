e3dd677f39cc604dabfe08fcc832d374c47ab6e0
ProjectInfinity <asjur@proton.me>
2025-04-14 20:58:52 +0200

    Refactor api.ts

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,11 +2,13 @@ import {getToken} from "$stores/UserStore.svelte";
 import {getVersion} from "$stores/ApplicationStore.svelte";
 
 const API_URL = 'https://aac.gaijin.dev';
-let DEFAULT_HEADERS = {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-    'X-Client': getVersion(),
-    'X-Token': ''
+function createHeaders(): Record<string, string> {
+    return {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'X-Client': getVersion(),
+        'X-Token': getToken()
+    };
 }
 
 export const apiClient = {
@@ -15,17 +17,16 @@ export const apiClient = {
 }
 
 async function get(url: string): Promise<Response> {
-    DEFAULT_HEADERS['X-Token'] = getToken();
-    return await fetch(API_URL + url,
-        {method: 'GET', headers: DEFAULT_HEADERS}
-    );
+    return fetch(API_URL + url, {
+        method: 'GET',
+        headers: createHeaders()
+    });
 }
 
-async function post(url: string, data: object): Promise<Response> {
-    DEFAULT_HEADERS['X-Token'] = getToken();
-    return await fetch(API_URL + url, {
+async function post(url: string, data: unknown): Promise<Response> {
+    return fetch(API_URL + url, {
         method: 'POST',
-        body: JSON.stringify(data),
-        headers: DEFAULT_HEADERS
+        headers: createHeaders(),
+        body: JSON.stringify(data)
     });
 }
