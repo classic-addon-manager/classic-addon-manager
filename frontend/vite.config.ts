7f219debee33e02f0aeac6603b7785ac4f0190ba
ProjectInfinity <asjur@proton.me>
2025-07-30 00:48:46 +0200

    chore: update vite config 6 and disable SSR

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,15 @@
-// @ts-ignore
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import {svelte} from '@sveltejs/vite-plugin-svelte'
 // @ts-ignore
 import path from 'path'
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  plugins: [svelte()],
+export default defineConfig(({mode}) => ({
+  plugins: [svelte({
+    compilerOptions: {}
+  })],
   resolve: {
-    conditions: mode === 'test' ? ['browser'] : [],
+    conditions: ['browser'],
     alias: {
       '@': path.resolve('./src'),
       $lib: path.resolve('./src/lib'),
@@ -16,5 +17,18 @@ export default defineConfig(({ mode }) => ({
       $screens: path.resolve('./src/screens'),
       $atoms: path.resolve('./src/atoms'),
     },
+  },
+  ssr: {
+    noExternal: []
+  },
+  optimizeDeps: {
+    include: ['svelte']
+  },
+  build: {
+    target: 'esnext',
+    ssr: false
+  },
+  define: {
+    'import.meta.env.SSR': false
   }
 }))
