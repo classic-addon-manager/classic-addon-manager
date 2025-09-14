f26e9911cadb4a64d6905b0d1b3b38c7bb9cdb4b
ProjectInfinity <asjur@proton.me>
2025-09-14 02:18:01 +0200

    refactor(remote_addons): simplify main functions by extracting logic into helpers

--- backend/addon/remote_addons.go
+++ backend/addon/remote_addons.go
@@ -17,43 +17,13 @@ import (
 )
 
 func InstallAddon(manifest shared.AddonManifest, version string) (bool, error) {
-	if !file.FileExists(filepath.Join(config.GetAddonDir(), "addons.txt")) {
-		logger.Info("addons.txt not found in AAC path, creating it.")
-		CreateAddonsTxt()
-	}
+	ensureAddonsTxtExists()
 
 	logger.Info("Installing addon:" + manifest.Name + " from " + manifest.Repo + " version: " + version)
 
-	zipName := manifest.Name + ".zip"
-
-	var url string
-	if version == "" || version == "latest" {
-		url = fmt.Sprintf("/addon/%s/download", manifest.Name)
-	} else {
-		url = fmt.Sprintf("/addon/%s/download?version=%s", manifest.Name, version)
-	}
-
-	err := util.DownloadFile(api.ApiURL+url, filepath.Join(config.GetCacheDir(), zipName))
-	if err != nil {
+	if err := downloadAndExtractAddon(manifest, version); err != nil {
 		return false, err
 	}
-
-	if file.FileExists(filepath.Join(config.GetCacheDir(), manifest.Name)) {
-		// Remove old addon release so we don't copy possibly removed files over
-		err = os.RemoveAll(filepath.Join(config.GetCacheDir(), manifest.Name))
-		if err != nil {
-			logger.Error("Error removing old addon release:", err)
-			return false, err
-		}
-	}
-
-	err = util.ExtractAddonRelease(zipName, manifest.Name)
-	if err != nil {
-		logger.Error("Error extracting addon release:", err)
-		return false, err
-	}
-
-	_ = os.Remove(filepath.Join(config.GetCacheDir(), zipName))
 
 	if !util.MoveAddonRelease(manifest.Name) {
 		logger.Error(manifest.Name+" - Error moving addon release", errors.New("error moving addon release"))
@@ -64,16 +34,9 @@ func InstallAddon(manifest shared.AddonManifest, version string) (bool, error) {
 		return false, err
 	}
 
-	if version == "" {
-		version = "latest"
-	}
-
-	release, err := api.GetAddonRelease(manifest.Name, version)
-	if err != nil {
+	if err := updateAddonMetadata(manifest, version); err != nil {
 		return false, err
 	}
-
-	AddManagedAddon(manifest, release)
 
 	logger.Info(manifest.Name + " installed successfully")
 	return true, nil
@@ -81,138 +44,25 @@ func InstallAddon(manifest shared.AddonManifest, version string) (bool, error) {
 
 // UpdateAddon updates an existing addon by replacing all files except the persistent .data folder.
 func UpdateAddon(manifest shared.AddonManifest, version string) (bool, error) {
-	if !file.FileExists(filepath.Join(config.GetAddonDir(), "addons.txt")) {
-		logger.Info("addons.txt not found in AAC path, creating it.")
-		CreateAddonsTxt()
-	}
+	ensureAddonsTxtExists()
 
 	logger.Info("Updating addon:" + manifest.Name + " from " + manifest.Repo + " version: " + version)
 
-	zipName := manifest.Name + ".zip"
-
-	var url string
-	if version == "" || version == "latest" {
-		url = fmt.Sprintf("/addon/%s/download", manifest.Name)
-	} else {
-		url = fmt.Sprintf("/addon/%s/download?version=%s", manifest.Name, version)
-	}
-
-	// Download the release zip into cache
-	err := util.DownloadFile(api.ApiURL+url, filepath.Join(config.GetCacheDir(), zipName))
-	if err != nil {
+	if err := downloadAndExtractAddon(manifest, version); err != nil {
 		return false, err
 	}
 
-	// Clear any previous extracted release in cache
-	cacheExtractDir := filepath.Join(config.GetCacheDir(), manifest.Name)
-	if file.FileExists(cacheExtractDir) {
-		if err = os.RemoveAll(cacheExtractDir); err != nil {
-			logger.Error("Error removing old extracted addon release:", err)
-			return false, err
-		}
-	}
-
-	// Extract the new release into cache
-	if err = util.ExtractAddonRelease(zipName, manifest.Name); err != nil {
-		logger.Error("Error extracting addon release:", err)
+	if err := performUpdateFileOperations(manifest); err != nil {
 		return false, err
 	}
-	_ = os.Remove(filepath.Join(config.GetCacheDir(), zipName))
-
-	// Find the root directory of the extracted release (first directory inside cacheExtractDir)
-	entries, err := os.ReadDir(cacheExtractDir)
-	if err != nil {
-		logger.Error("Error reading extracted release directory:", err)
-		return false, err
-	}
-
-	var rootDir string
-	for _, e := range entries {
-		if e.IsDir() {
-			rootDir = e.Name()
-			break
-		}
-	}
-	if rootDir == "" {
-		logger.Error(manifest.Name+" - No root directory found in addon release, aborting", errors.New("no root directory found"))
-		return false, nil
-	}
-
-	destAddonDir := filepath.Join(config.GetAddonDir(), manifest.Name)
-
-	// Ensure destination exists
-	if err := os.MkdirAll(destAddonDir, os.ModePerm); err != nil {
-		logger.Error("Error creating destination addon directory:", err)
-		return false, err
-	}
-
-	// Remove everything in destination except the .data folder
-	destEntries, err := os.ReadDir(destAddonDir)
-	if err != nil {
-		logger.Error("Error reading destination addon directory:", err)
-		return false, err
-	}
-	for _, de := range destEntries {
-		name := de.Name()
-		if name == ".data" {
-			continue
-		}
-		removePath := filepath.Join(destAddonDir, name)
-		if err := os.RemoveAll(removePath); err != nil {
-			logger.Error("Error removing path during update: ", err)
-			return false, err
-		}
-	}
-
-	// Copy new release contents into destination, skipping .data to preserve it
-	srcRoot := filepath.Join(cacheExtractDir, rootDir)
-	srcEntries, err := os.ReadDir(srcRoot)
-	if err != nil {
-		logger.Error("Error reading source release root:", err)
-		return false, err
-	}
-	for _, se := range srcEntries {
-		name := se.Name()
-		if name == ".data" {
-			// Do not copy .data from the release, keep existing persistent data
-			continue
-		}
-		srcPath := filepath.Join(srcRoot, name)
-		destPath := filepath.Join(destAddonDir, name)
-		if se.IsDir() {
-			if err := file.CopyDir(srcPath, destPath); err != nil {
-				logger.Error("Error copying updated addon directory:", err)
-				return false, err
-			}
-		} else {
-			// Ensure destination directory exists then copy file
-			if mkErr := os.MkdirAll(filepath.Dir(destPath), os.ModePerm); mkErr != nil {
-				logger.Error("Error creating destination path:", mkErr)
-				return false, mkErr
-			}
-			if err := file.MoveFile(srcPath, destPath); err != nil {
-				logger.Error("Error copying updated addon file:", err)
-				return false, err
-			}
-		}
-	}
-
-	// Cleanup extracted cache
-	_ = os.RemoveAll(cacheExtractDir)
 
 	if err := AddToAddonsTxt(manifest.Name); err != nil {
 		return false, err
 	}
 
-	// Update managed addon metadata
-	if version == "" {
-		version = "latest"
-	}
-	release, err := api.GetAddonRelease(manifest.Name, version)
-	if err != nil {
+	if err := updateAddonMetadata(manifest, version); err != nil {
 		return false, err
 	}
-	AddManagedAddon(manifest, release)
 
 	logger.Info(manifest.Name + " updated successfully")
 	return true, nil
@@ -248,4 +98,127 @@ func GetAddonManifest() []shared.AddonManifest {
 	logger.Info("Retrieved " + strconv.Itoa(len(manifests)) + " addon manifests from remote source")
 
 	return manifests
+}
+
+func ensureAddonsTxtExists() {
+	if !file.FileExists(filepath.Join(config.GetAddonDir(), "addons.txt")) {
+		logger.Info("addons.txt not found in AAC path, creating it.")
+		CreateAddonsTxt()
+	}
+}
+
+func buildDownloadURL(manifest shared.AddonManifest, version string) string {
+	if version == "" || version == "latest" {
+		return fmt.Sprintf("/addon/%s/download", manifest.Name)
+	}
+	return fmt.Sprintf("/addon/%s/download?version=%s", manifest.Name, version)
+}
+
+func downloadAndExtractAddon(manifest shared.AddonManifest, version string) error {
+	zipName := manifest.Name + ".zip"
+	url := buildDownloadURL(manifest, version)
+
+	if err := util.DownloadFile(api.ApiURL+url, filepath.Join(config.GetCacheDir(), zipName)); err != nil {
+		return err
+	}
+
+	if file.FileExists(filepath.Join(config.GetCacheDir(), manifest.Name)) {
+		if err := os.RemoveAll(filepath.Join(config.GetCacheDir(), manifest.Name)); err != nil {
+			return err
+		}
+	}
+
+	if err := util.ExtractAddonRelease(zipName, manifest.Name); err != nil {
+		return err
+	}
+
+	return os.Remove(filepath.Join(config.GetCacheDir(), zipName))
+}
+
+func updateAddonMetadata(manifest shared.AddonManifest, version string) error {
+	if version == "" {
+		version = "latest"
+	}
+
+	release, err := api.GetAddonRelease(manifest.Name, version)
+	if err != nil {
+		return err
+	}
+
+	AddManagedAddon(manifest, release)
+	return nil
+}
+
+func performUpdateFileOperations(manifest shared.AddonManifest) error {
+	cacheExtractDir := filepath.Join(config.GetCacheDir(), manifest.Name)
+
+	// Find the root directory of the extracted release
+	entries, err := os.ReadDir(cacheExtractDir)
+	if err != nil {
+		return err
+	}
+
+	var rootDir string
+	for _, e := range entries {
+		if e.IsDir() {
+			rootDir = e.Name()
+			break
+		}
+	}
+	if rootDir == "" {
+		return errors.New("no root directory found")
+	}
+
+	destAddonDir := filepath.Join(config.GetAddonDir(), manifest.Name)
+
+	// Ensure destination exists
+	if err := os.MkdirAll(destAddonDir, os.ModePerm); err != nil {
+		return err
+	}
+
+	// Remove everything in destination except the .data folder
+	destEntries, err := os.ReadDir(destAddonDir)
+	if err != nil {
+		return err
+	}
+	for _, de := range destEntries {
+		name := de.Name()
+		if name == ".data" {
+			continue
+		}
+		removePath := filepath.Join(destAddonDir, name)
+		if err := os.RemoveAll(removePath); err != nil {
+			return err
+		}
+	}
+
+	// Copy new release contents into destination, skipping .data
+	srcRoot := filepath.Join(cacheExtractDir, rootDir)
+	srcEntries, err := os.ReadDir(srcRoot)
+	if err != nil {
+		return err
+	}
+	for _, se := range srcEntries {
+		name := se.Name()
+		if name == ".data" {
+			continue
+		}
+		srcPath := filepath.Join(srcRoot, name)
+		destPath := filepath.Join(destAddonDir, name)
+		if se.IsDir() {
+			if err := file.CopyDir(srcPath, destPath); err != nil {
+				return err
+			}
+		} else {
+			if mkErr := os.MkdirAll(filepath.Dir(destPath), os.ModePerm); mkErr != nil {
+				return mkErr
+			}
+			if err := file.MoveFile(srcPath, destPath); err != nil {
+				return err
+			}
+		}
+	}
+
+	// Cleanup extracted cache
+	return os.RemoveAll(cacheExtractDir)
 }
